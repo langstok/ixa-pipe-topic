cbbcfeb9572a1767cbc8792133fdd6b97d10cf67
Sander Puts <putssander@gmail.com>
2016-10-23 11:15:15 +0200

    reduced processing time to 100ms (factor 30) test for threadsafety (maybe use randomness for filewriting)

--- src/main/java/ixa/pipe/topic/IndexNAF.java
+++ src/main/java/ixa/pipe/topic/IndexNAF.java
@@ -1,6 +1,7 @@
 package ixa.pipe.topic;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
@@ -12,31 +13,39 @@ import it.jrc.lt.evidx.Utils;
 public class IndexNAF {
 
 	private Dictionary dict;
+	private CalculateClassifiers cal;
+	private File outFile;
 
 	public IndexNAF(Properties properties){
 		this.dict = new Dictionary(properties.getProperty(Utils.DICTIONARY));
-		dict.load();
+		this.dict.load();
+		try {
+			this.outFile = File.createTempFile("assignTmp", ".xml", new File("/tmp"));
+			properties.setProperty("output", this.outFile.getAbsolutePath());
+			this.cal = new CalculateClassifiers(properties);
+		} catch (IOException e) {
+			LOGGER.error("Error init CalculateClassifiers", e);
+		}
+		
+		LOGGER.info("Building Classifier done");
 	}
 
 	private static final Logger LOGGER = Logger.getLogger(IndexNAF.class);
 
 	/* Based on assign function from EuroVocExecuter class */
 	public File assign (File input, Properties properties) throws Exception {
-		LOGGER.info("Start indexing document");
 		
-		File outFile = File.createTempFile("assignTmp", ".xml", new File("/tmp"));
-		properties.setProperty("output", outFile.getAbsolutePath());
-		
-		CalculateClassifiers cal = new CalculateClassifiers(properties);
-		LOGGER.info("Building Classifier done");
-
+		LOGGER.info("Create DocumentSet");
 		File documentSet = File.createTempFile("EuroVocDocSet", "tmp");
-		documentSet.deleteOnExit();
-
 		Utils.createDocumentSet(input, dict, documentSet);
+		
+		LOGGER.info("Assign input");
 		cal.doAssigning(documentSet);
-		LOGGER.info("Building Documentset done");
-		return outFile;
+		
+		LOGGER.info("Delete DocumentSet");
+		documentSet.delete();
+
+		return this.outFile;
 	}
 	
 	
